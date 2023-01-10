f75c6e81aed94a0c13d00efe980561ae75876f76
unknown <pvuganeza@gmail.com>
2023-01-10 00:18:04 -0800

    adding new path

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = FastAPI()# variable name of the FastAPI instance
 @app.get("/") # decorator: path/route
 def index():
     return "This is the front page"
+
+@app.get("/about")
+def about():
+    return "This is the about page"
+
 # pip install fastapi uvicorn
 # uvicorn app:app --reload: run the app==>app.py:app==>app.py is the file name, app is the variable name of the FastAPI instance
+
+# render app name: uvicorn app:app --host 0.0.0.0 --port 10000
